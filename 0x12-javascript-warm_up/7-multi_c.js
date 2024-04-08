bacec99ff7b83221cfa2b66e028481cf023dc003
adham <adhamalpap27@gmail.com>
2024-04-08 13:33:59 +0000

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,6 +2,6 @@
 if (isNaN(parseInt(`${process.argv[2]}`))) {
   console.log("Missing number of occurrences");
 } else {
-  console.log("adham\n".repeat(process.argv[2] - 1) + "adham");
+  console.log("C is fun\n".repeat(process.argv[2] - 1) + "C is fun");
 }
 
